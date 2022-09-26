a2d2c9f0314498757f7667540d18a6f5e2b90ae4
Adrien WHP <adrien@webhorspiste.com>
2022-09-26 16:38:18 +0200

    Ajout animation resize)

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import "./About.css";
+import aboutImg from '../../images/about-img.jpeg';
 
 const About = () => {
   return (
-    <div>
-        about
-    </div>
+    <section className='about'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>About</h2>
+        </div>
+        <div className='about-content grid'>
+          <div className='about-img'>
+            <img src={aboutImg} alt=""/>
+          </div>
+          <div className='about-text'>
+            <h2 className='about-title fs-26 ls-1'>About Kili</h2>
+            <p className='fs-17'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod tempore debitis molestias repellendus nobis quaerat quia error in, natus cumque saepe atque delectus iure ipsum provident ea eum architecto, eaque totam qui. Nesciunt sunt iusto quod! Repellat asperiores possimus dolor. Ea voluptatibus adipisci minus sit distinctio! Nemo voluptates accusantium nam!</p>
+            <p className='fs-17'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod tempore debitis molestias repellendus nobis quaerat quia error in, natus cumque saepe atque delectus iure ipsum provident ea eum architecto, eaque totam qui. Nesciunt sunt iusto quod! Repellat asperiores possimus dolor. Ea voluptatibus adipisci minus sit distinctio! Nemo voluptates accusantium nam!</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
