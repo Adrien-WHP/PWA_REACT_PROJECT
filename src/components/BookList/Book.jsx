73e55d91499f9c86880cb951b93750b03c7eeb07
Adrien WHP <adrien@webhorspiste.com>
2022-09-26 16:16:33 +0200

    Component finish for fetch book et display them

--- src/components/BookList/Book.jsx
+++ src/components/BookList/Book.jsx
@@ -9,21 +9,21 @@ const Book = (book) => {
         <img src = {book.cover_img} alt='cover'/>
       </div>
       <div className='book-item-info text-center'>
-        <Link to = {`/book/${book.id}`} {... book}>
+        <Link to = {`/book${book.id}`} {... book}>
           <div className='book-item-info-item title fw-7 fs-18'>
             <span>{book.title}</span>
           </div>
         </Link>
         <div className='book-item-info-item author fs-15'>
-          <span className='texte-capitalize fw-7'>Author : </span>
-          <span>{book.author.join(",")}</span>
+          <span className='texte-capitalize fw-7'>Auteur : </span>
+          <span>{book.author.join(', ')}</span>
         </div>
         <div className='book-item-info-item edition-count fs-15'>
           <span className='texte-capitalize fw-7'>Total Editions : </span>
-          <span>{book.author.join(",")}</span>
+          <span>{book.author.join(', ')}</span>
         </div>
         <div className='book-item-info-item publish-year fs-15'>
-          <span className='text-capitalize fw-7'>First Publish Year</span>
+          <span className='text-capitalize fw-7'>First Publish Year : </span>
           <span>{book.first_publish_year}</span>
         </div>
       </div>
